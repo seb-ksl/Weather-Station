f403147c67109a2087832996096fbaa004f6e1cd
seb-ksl <keselbingo@gmail.com>
2016-09-14 11:50:10 +0200

    Allow NTP output

--- firewall.sh
+++ firewall.sh
@@ -29,6 +29,7 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
 iptables -t filter -P INPUT DROP
